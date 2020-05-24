c5207122a955d39aba1012110be46055453137d8
Pedro Marin <pmarinr@gmail.com>
2020-05-24 20:10:16 +0200

    Pulpo, baul y caja fuerte

---
+++ Scripts/OnSnap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnSnap : MonoBehaviour
+{
+    private bool _inSnap = false;
+
+    private Rigidbody _rb;
+  
+    void Start()
+    {
+        _rb = this.GetComponent<Rigidbody>();
+    }
+
+    public void ChangeKinematic(bool iskine)
+    {
+        _rb.isKinematic = _inSnap;
+    }
+
+    public void InSnap(bool insnap)
+    {
+        _inSnap = insnap;
+    }
+}

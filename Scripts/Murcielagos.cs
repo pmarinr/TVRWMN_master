f4d199b5fa80a604c38517f08f3084f621fd45fb
Pedro Marin <pmarinr@gmail.com>
2020-05-28 10:53:56 +0200

    Murcielagos

--- Scripts/Murcielagos.cs
+++ Scripts/Murcielagos.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Murcielagos : MonoBehaviour
 {
     public Transform bats;
-    public Transform entrada, centro;
+    public Transform entrada, centro, salida;
     public int vueltas = 4;
     public float tiempo = 10;
 
@@ -23,14 +23,21 @@ public class Murcielagos : MonoBehaviour
 
     IEnumerator InvokeBats()
     {
-        yield return new WaitForSeconds(5f);
-        bats.transform.DOMove(entrada.position, 1);
+        
         yield return new WaitForSeconds(1f);
-        //centro.transform.DORotate(new Vector3(0, 360*vueltas, 0), tiempo, RotateMode.FastBeyond360);
-        bats.transform.transform.DOSpiral(3, Vector3.forward, SpiralMode.ExpandThenContract, 0.5f, 5);
+        bats.gameObject.SetActive(true);
+        Tween myTween = bats.transform.DOMove(entrada.position, 1);
+        yield return myTween.WaitForCompletion();
+        myTween = centro.transform.DORotate(new Vector3(0, 360, 0), 2, RotateMode.FastBeyond360);
+        //bats.transform.transform.DOSpiral(3, Vector3.forward, SpiralMode.ExpandThenContract, 0.5f, 5);
 
-        yield return new WaitForSeconds(tiempo);
-        bats.transform.DOMove(inicio.position, 1);
-
+        yield return myTween.WaitForCompletion();
+        myTween = centro.transform.DORotate(new Vector3(0, 360, 0), 4, RotateMode.FastBeyond360);
+        yield return myTween.WaitForCompletion();
+        myTween = centro.transform.DORotate(new Vector3(0, 360, 0), 4, RotateMode.FastBeyond360);
+        yield return myTween.WaitForCompletion();
+        myTween = bats.transform.DOMove(salida.position, 1);
+        yield return myTween.WaitForCompletion();
+        bats.gameObject.SetActive(false);
     }
 }

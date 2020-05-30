234cb579e101308fb1715e02e7267ecb4fa1cf9f
Pedro Marin <pmarinr@gmail.com>
2020-05-30 15:52:56 +0200

    MultiplayerFunctions

---
+++ Scripts/MultiplayerFunctions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Pun.UtilityScripts;
+using Photon.Realtime;
+
+public class MultiplayerFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse down");
+            ApagaLuz(2);
+        }
+    }
+
+    [PunRPC]
+    public void Apaga(int luz)
+    {
+        Debug.Log("Funcion APAGA: " + luz);
+
+    }
+
+    public void ApagaLuz(int luz)
+    {
+        PhotonView photonView = PhotonView.Get(this);
+        photonView.RPC("Apaga", RpcTarget.All, luz);
+    }
+
+
+}

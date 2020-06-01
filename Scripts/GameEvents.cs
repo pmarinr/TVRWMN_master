805b6bbaec2a5ccbc3c08f1163ac17873adc689b
Pedro Marin <pmarinr@gmail.com>
2020-06-01 12:08:41 +0200

    GameEvents

--- Scripts/GameEvents.cs
+++ Scripts/GameEvents.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Photon.Pun;
+using Photon.Pun.UtilityScripts;
+using Photon.Realtime;
 public class GameEvents : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -14,14 +16,24 @@ public class GameEvents : MonoBehaviour
     public event System.Action onLightOff;
     public event System.Action<string> onLightOn;
 
+    [PunRPC]
     public void ApagarLuces()
     {
         onLightOff();
     }
 
+    [PunRPC]
     public void EncenderLuz(string nombre)
     {
         onLightOn(nombre);
+    }
+
+    
+
+    public void EncenderLuzRPC(string nombre)
+    {
+        PhotonView photonView = PhotonView.Get(this);
+        photonView.RPC("EncenderLuz", RpcTarget.All, nombre);
     }
 
 }
